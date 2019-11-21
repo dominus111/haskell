bd13cbd5c9e5d065fc92a069cdd8a6a87c92ee03
dominus111 <48447356+dominus111@users.noreply.github.com>
2019-11-21 23:08:43 +0000

    An approximation to the real square root of d
    Babylonian method

---
+++ approxSqrt.hs
@@ -0,0 +1,23 @@
+--TEMPLATE FILE FOR COURSEWORK 1 for COMP2209
+--Julian Rathke, Oct 2019
+
+--EXERCISE A2 ONLY
+
+--CONTAINS FUNCTION REQIURED FOR COMPILATION AGAINST THE TEST SUITE
+--MODIFY THE FUNCTION DEFINITIONS WITH YOUR OWN SOLUTIONS
+--IMPORTANT : DO NOT MODIFY ANY FUNCTION TYPES
+
+
+module Exercises (approxSqrt) where
+
+-- Exercise A2
+approxSqrt :: Double -> Double -> Double
+--approxSqrt d eps = -1
+approxSqrt d eps
+    | d < 0 = error "Invalid number"
+    | eps <= 0 = error "Invalid number"
+    | otherwise = approxSqrt' d eps ((1 + d) / 2)
+
+approxSqrt' :: Double -> Double -> Double -> Double
+approxSqrt' d eps x | abs((sqrt d) - x) < eps = x
+                    | otherwise = approxSqrt' d eps (x + d/x) / 2
