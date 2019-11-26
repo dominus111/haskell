5957e614dab22ab096b20fca1f40abefb62088c5
dominus111 <48447356+dominus111@users.noreply.github.com>
2019-11-26 17:25:13 +0000

    working correctly

--- histogram.hs
+++ histogram.hs
@@ -13,14 +13,9 @@ module Exercises (histogram) where
 -- Exercise A1
 histogram :: Int -> [Int] -> [Int]
 histogram n [] = error "Empty List"
-histogram n xs = [pairLength m xs | m <- hisRange n xs]
+histogram n xs | n <= 0  = error "Invalid input"
+               |otherwise = [pairLength m xs | m <- hisRange n xs]
 
 pairLength m xs =  length[x| x <-xs, (fst m) <= x , x <= (snd m)]
 hisRange n xs = zip [0,n .. k*n] [n-1, 2*n-1 .. (n*(k+1)-1)]
  where k = maximum(xs)`div`n
-
-
-
-
-
-
